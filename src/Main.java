5f8de67249fbc765f9256dd1d68bb6dcfc584194
Halima-Sayed <halimasayed1999@gmail.com>
2023-07-09 01:42:51 -0400

    the feature Sun has been added sucessfully

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,10 @@ public class Main {
         Omanx.Planets="2";
         Omanx.Star="4";
 
+        FeatureSun Xeon=new FeatureSun();
+        Xeon.name="Treo";
+        Xeon.size="32432km";
+        Xeon.temp="344449843 celcius";
+
     }
 }
